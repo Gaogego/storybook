159a5296db8e83eb8280102aeab649424553065d
Alex <alex.minghui.xu@gmail.com>
2022-08-08 23:00:11 -0400

    express and mongodb prepared. running checked

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDb = require("./config/db");
+
+// load config
 require("dotenv").config({ path: "./config/config.env" });
+// connectDb
+connectDb();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDb = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.conection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(error);
     process.exit(1);
